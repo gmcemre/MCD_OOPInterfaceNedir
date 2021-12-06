1981b5eafc990b3319bf983c33d29f992f121d61
emre gemici <gmcc.emree@gmail.com>
2021-12-06 16:46:18 +0300

    Add project files.

---
+++ MCD_OOPInterfaceNedir/Imusteri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPInterfaceNedir
+{
+    public interface Imusteri
+    {
+        //public Imusteri()
+        //{
+        //      interface içerisinde ctor kullanılmaz...
+        //}
+
+        //field da kullanılamaz..
+
+        //Properties
+        int Id { get; set; }
+        string isim { get; set; }
+        string soyisim { get; set; }
+
+        //metot
+        int yeniKayit(string isim, string soyisim);
+        int kayitDuzenle(int id,string isim, string soyisim);
+        int kayitSil(int id);
+
+    }
+}
---
+++ MCD_OOPInterfaceNedir/musteri.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPInterfaceNedir
+{
+    public class musteri : Imusteri
+    {
+        int _id;
+        string _isim;
+        string _soyisim;
+        int Imusteri.Id 
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value; 
+            }
+        }
+        string Imusteri.isim 
+        {
+            get
+            {
+                return _isim;
+            }
+            set
+            {
+                _isim = value;
+            }
+        }
+        string Imusteri.soyisim 
+        {
+            get
+            {
+                return _soyisim;
+            }
+            set
+            {
+                _soyisim = value;
+            }
+        }
+
+        int Imusteri.kayitDuzenle(int id, string isim, string soyisim)
+        {
+            Console.WriteLine("Kayıt Düzenlendi...");
+            return 1;
+        }
+
+        int Imusteri.kayitSil(int id)
+        {
+            Console.WriteLine("Kayıt Silindi...");
+            return 1;
+        }
+
+        int Imusteri.yeniKayit(string isim, string soyisim)
+        {
+            Console.WriteLine("Yeni Kayıt Eklendi...");
+            return 1;
+        }
+    }
+}
